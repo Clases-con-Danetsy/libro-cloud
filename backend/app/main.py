f2d871ceebe08d85c542b4280acaf19299562bc2
Cristian MeSu <cristian.mendoza.s@hotmail.com>
2025-12-31 15:36:39 -0500

    feat: add new dependencies to the frontend project

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,11 +2,22 @@ from fastapi import FastAPI
 from app.database import SessionLocal, engine, Base
 from app.crud import crear_rol_inicial, crear_usuario_inicial
 from app.controllers import usuario_router, rol_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Libro Cloud API")
 
+# 🔥 Agregar CORS ANTES de los routers
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4321"],  # Puerto de Astro
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 print("🔧 Creando tablas si no existen...")
 Base.metadata.create_all(bind=engine)
+
 
 @app.on_event("startup")
 def startup_event():
@@ -18,9 +29,11 @@ def startup_event():
     finally:
         db.close()
 
+
 @app.get("/")
 def read_root():
     return {"message": "Libro Cloud API Funcionando Correctamente"}
+
 
 # Incluir routers de los controladores
 app.include_router(usuario_router)
